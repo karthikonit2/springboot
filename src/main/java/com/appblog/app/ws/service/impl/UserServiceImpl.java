5f4fb7eea85b9ef3155d60b1f063a90d67528d3f
Bhargav, Gireesh <gireesh.bhargav@one.verizon.com>
2020-06-08 18:09:19 -0500

    random userid generated

--- src/main/java/com/appblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appblog/app/ws/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.appblog.app.ws.service.impl;
 import com.appblog.app.ws.io.repository.UserRepository;
 import com.appblog.app.ws.io.entity.UserEntity;
 import com.appblog.app.ws.service.UserService;
+import com.appblog.app.ws.shared.Utils;
 import com.appblog.app.ws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +17,24 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
+
+        if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exist");
 
         UserEntity userEntity = new UserEntity();
 
         BeanUtils.copyProperties(user, userEntity);
 
-         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
+        String publicUserId = utils.generateUserId(30);
+
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId(publicUserId);
+
+
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
 
